1d91126dd79adc68f822bcb8dbfdcf6be8fafc5e
Zahra <hamdane.zahra@gmail.com>
2023-01-03 21:04:57 +0100

    handling Exception

--- src/main/java/com/ensa/mscustomer/controllers/CustomerController.java
+++ src/main/java/com/ensa/mscustomer/controllers/CustomerController.java
@@ -2,6 +2,8 @@ package com.ensa.mscustomer.controllers;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,20 +13,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ensa.mscustomer.exceptions.CustomerException;
 import com.ensa.mscustomer.requests.CustomerRequest;
 import com.ensa.mscustomer.responses.CustomerResponse;
+import com.ensa.mscustomer.responses.ErrorMessages;
 import com.ensa.mscustomer.services.CustomerService;
 import com.ensa.mscustomer.shared.dto.CustomerDto;
 
 @RestController
-@RequestMapping("/customers") // localhost:8080/customers
+@RequestMapping("/customers") // localhost:8088/customers
 public class CustomerController {
 	
 	@Autowired
 	CustomerService customerService; // l'injection de depondance 
 	
 	@GetMapping(path="/{id}")
-	public CustomerResponse getCustomer(@PathVariable String id) {
+	public ResponseEntity<CustomerResponse> getCustomer(@PathVariable String id) {
 		
 		CustomerDto customerDto = customerService.getCustomerByCustomerId(id);
 		
@@ -32,11 +36,13 @@ public class CustomerController {
 		
 		BeanUtils.copyProperties(customerDto, customerResponse);
 		
-		return customerResponse;
+		return new ResponseEntity<CustomerResponse>(customerResponse, HttpStatus.OK); 
 	}
 	
 	@PostMapping
-	public CustomerResponse createCustomer(@RequestBody CustomerRequest customerRequest) {
+	public ResponseEntity<CustomerResponse> createCustomer(@RequestBody CustomerRequest customerRequest) throws Exception{
+		
+		if(customerRequest.getFirstName().isEmpty()) throw new CustomerException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
 		
 		CustomerDto customerDto = new CustomerDto();
 		BeanUtils.copyProperties(customerRequest, customerDto); // la liaison entre la couche presentation et la couche service
@@ -48,13 +54,12 @@ public class CustomerController {
 		
 		BeanUtils.copyProperties(createCustomer, customerResponse);
 		
-		return customerResponse;
-		
+		return new ResponseEntity<CustomerResponse>(customerResponse, HttpStatus.CREATED);  
 		
 	}
 	
 	@PutMapping(path="{id}")
-	public CustomerResponse updateCustomer(@PathVariable String id, @RequestBody CustomerRequest customerRequest) {
+	public ResponseEntity<CustomerResponse> updateCustomer(@PathVariable String id, @RequestBody CustomerRequest customerRequest) {
 
 		CustomerDto customerDto = new CustomerDto();
 		BeanUtils.copyProperties(customerRequest, customerDto); // la liaison entre la couche presentation et la couche service
@@ -66,15 +71,14 @@ public class CustomerController {
 		
 		BeanUtils.copyProperties(updateCustomer, customerResponse);
 		
-		return customerResponse;
+		return new ResponseEntity<CustomerResponse>(customerResponse, HttpStatus.ACCEPTED);  
 	}
 	
 	@DeleteMapping(path="{id}")
-	public String deleteCustomer(@PathVariable String id) {
+	public ResponseEntity<Object> deleteCustomer(@PathVariable String id) {
 
 		customerService.deleteCustomer(id);
 		
-		return "Custumer suprimer";
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
-
 }
---
+++ src/main/java/com/ensa/mscustomer/exceptions/CustomerException.java
@@ -0,0 +1,18 @@
+package com.ensa.mscustomer.exceptions;
+
+public class CustomerException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5619900237990238555L;
+
+	public CustomerException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+
+}
